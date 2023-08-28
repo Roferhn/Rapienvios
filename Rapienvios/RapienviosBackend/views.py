b56b027bcb6506d41e07a1fedec2fa7882a0d34b
Rofer <roferhn@gmail.com>
2023-08-28 10:36:31 -0600

    Get Packages View

--- Rapienvios/RapienviosBackend/views.py
+++ Rapienvios/RapienviosBackend/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import JsonResponse
+from .models import *
+from .serializers import *
 
 # Create your views here.
+
+class PackageListview(View):
+    
+    def get(self, request):
+        packages = Package.objects.all()
+        serializer = PackageSerializer(packages, many = True)
+
+        data= serializer.data
+
+        return JsonResponse(data, safe=False)
