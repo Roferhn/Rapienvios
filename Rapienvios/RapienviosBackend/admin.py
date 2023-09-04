9dc6178e547a84369a861858a05c964f8ea1f0aa
Rofer <roferhn@gmail.com>
2023-09-04 09:53:49 -0600

    Define null atributes

--- Rapienvios/RapienviosBackend/admin.py
+++ Rapienvios/RapienviosBackend/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'name', 'lastname', 'phone', 'type')
+
+admin.site.register(Shipping_Status)
+admin.site.register(Shipping_Type)
+admin.site.register(User_Type)
+admin.site.register(User, UserAdmin)
+admin.site.register(Shipping)
+admin.site.register(Pricing)
+admin.site.register(Package)
+admin.site.register(Locker)
--- Rapienvios/RapienviosBackend/models.py
+++ Rapienvios/RapienviosBackend/models.py
@@ -5,73 +5,72 @@ from django.core.validators import MinLengthValidator
 
 # ---User Models--#
 class User_Type(models.Model):
-    type = models.CharField(max_length=50)
-    status = models.BooleanField(default=True)
+    type = models.CharField(max_length=50, null=False)
+    status = models.BooleanField(default=True, null=False)
 
     def __str__(self):
         return self.type
 
 class User(models.Model):
        
-    name = models.CharField(max_length=100)
-    lastname = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    password = models.TextField() #Investigar e implementar hashing
-    phone = models.CharField(validators=[MinLengthValidator(limit_value=8)], max_length=8)
-    user_type= models.ForeignKey(User_Type, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, null=False)
+    lastname = models.CharField(max_length=100, null= False)
+    email = models.EmailField(unique=True, null=False)
+    password = models.TextField(validators=[MinLengthValidator(limit_value=6)], null=False)
+    phone = models.CharField(validators=[MinLengthValidator(limit_value=8)], max_length=8, null=False)
+    type= models.ForeignKey(User_Type, on_delete=models.CASCADE)
     status = models.BooleanField(default=True)
     
     def __str__(self):
-        return self.type
+        return self.email
 
 # --- Shipping Models---#
 class Shipping_Status(models.Model):
-    status = models.CharField(max_length=100)
+    status = models.CharField(max_length=100, null=False)
 
     def __str__(self):
         return self.status
 
 class Shipping_Type(models.Model):
-    type = models.CharField(max_length=50)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    type = models.CharField(max_length=50, null=False)
 
     def __str__(self):
         return self.type
 
 class Shipping(models.Model):
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     shipping_date = models.DateField()
-    delicery_date = models.DateField()
+    delivery_date = models.DateField()
     updated_date = models.DateField()
-    shipping_type_id = models.ForeignKey(Shipping_Type, on_delete=models.CASCADE)
-    volume = models.DecimalField(max_digits=10, decimal_places=2)
-    weigth = models.DecimalField(max_digits=10, decimal_places=2)
-    shipping_status_id = models.ForeignKey(Shipping_Status, on_delete=models.CASCADE)
+    volume = models.DecimalField(max_digits=10, decimal_places=2, null=False)
+    weigth = models.DecimalField(max_digits=10, decimal_places=2, null=False)
+    type = models.ForeignKey(Shipping_Type, on_delete=models.CASCADE)
+    status = models.ForeignKey(Shipping_Status, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
-        return self.type
+        return self.status
 
 class Pricing(models.Model):
-    shipping_status_id = models.ForeignKey(Shipping_Status, on_delete=models.CASCADE)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    ShippingType = models.ForeignKey(Shipping_Type, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=False)
     initial_date = models.DateField()
     final_date = models.DateField()
 
     def __str__(self):
-        return self.type
+        return self.price
 
 # ---Package Models---#
 class Package(models.Model):
-    tracking_num = models.CharField(max_length=100)
-    reception_date = models.DateField(null=False)
-    packaged_date = models.DateField(null=False)
-    shipping_id = models.ForeignKey(Shipping, on_delete=models.CASCADE)
+    tracking_num = models.CharField(max_length=100,null=False)
+    reception_date = models.DateField()
+    packaged_date = models.DateField()
+    shipping_id = models.ForeignKey(Shipping, on_delete=models.CASCADE,null=True)
 
     def __str__(self):
         return self.type
 
 class Locker(models.Model):
-    package_id = models.ForeignKey(Package, on_delete=models.CASCADE)
+    package_id = models.ForeignKey(Package, on_delete=models.CASCADE, null=True)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
